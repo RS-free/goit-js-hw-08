73282870ccf245734826ef173282b711e2ebc96b
Ruslan Sulieimanov <ruslansulieim@gmail.com>
2021-12-19 23:31:26 +0200

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,14 +8,14 @@ const galleryBlock = document.querySelector('.gallery');
 
 const markupGallery = galleryItems
   .map(
-    ({ preview, original, description }) => `<li class="gallery__item">
+    ({ preview, original, description }) => `<div class="gallery__item">
     <a class="gallery__link" href="${original}">
     <img
     class="gallery__image"
     src="${preview}"
     alt="${description}"
     />
-    </a></li>`,
+    </a></div>`,
   )
   .join('');
 
